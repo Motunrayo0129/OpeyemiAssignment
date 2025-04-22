94208d9484e0a93d34c7056fe3cbd6384af619e3
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2025-04-22 18:09:08 +0100

    'git'

---
+++ myAssignment .java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+public class myAssignment {
+public static void main(String[] args) {
+	Scanner code = new Scanner(System.in);
+
+		int largestNumber = 0;
+		int count = 0;
+
+	while(counter != 5) {
+System.out.print("Enter a number: " (counter + 1));
+	int number = code.nextInt();
+if (largestNumber == number)
+	System.out.print("The largest number is", +largestNumber);
+
+
+
+}
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+} 
+}
